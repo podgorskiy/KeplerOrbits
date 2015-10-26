e3ad415f5b1260f58502ed5999fc553c3c2c660a
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2015-10-25 20:36:54 -0400

    - Clean of not needed stuff

--- src/Resources.h
+++ src/Resources.h
@@ -1,58 +1,34 @@
-#include "Planet.h"
-#include <tinyxml/tinyxml.h>
+#pragma once
+#include "KeplerOrbits/Body.h"
 #include <vector>
 
-#define toRad(x) (x/180.0*pi)
+class TiXmlHandle;
 
 class Resources
 {
 public:
-	typedef std::vector<Planet> Planets;
-	Resources(){};
-	~Resources(){};
-	void Load( const char* fileName ){
-		TiXmlDocument data( fileName );
-		data.LoadFile();
-		TiXmlHandle hData(&data);
-		TiXmlElement* pElem;
-		TiXmlHandle hRoot(0);
-		pElem=hData.FirstChild("data").Element();
-		if (!pElem) return;
-		hRoot=TiXmlHandle(pElem);
-		m_planets.clear();
-		pElem=hRoot.FirstChild( "planets" ).FirstChild().Element();
-		for( pElem; pElem; pElem=pElem->NextSiblingElement("planet") )
-		{
-			const char *pName=pElem->Attribute("name");
-			if (!pName) 
-			{
-				continue;
-			}
-			TiXmlHandle hPlanet = TiXmlHandle(pElem);
 
-			OrbitalElements orbit = OrbitalElements().
-				EllipseShape( GetVal( hPlanet, "Eccentricity" ), GetVal( hPlanet, "SemimajorAxis" ) ).
-				OrbitalPlane( GetRVal( hPlanet, "Inclination" ), GetRVal( hPlanet, "LongitudeOfAscendingNode" ) ).
-				EllipseOrientation( GetRVal( hPlanet, "LongitudeOfPerihelion" ) ).
-				TimeSpecific( GetRVal( hPlanet, "MeanLongitude" ), GetVal( hPlanet, "Epoch" ) );
-			
-			double siderealOrbitPeriod = GetVal( hPlanet, "SiderealOrbitPeriod" );
-			m_planets.push_back( Planet( pName, orbit, siderealOrbitPeriod ) );
-		}
+	struct RenderProperties
+	{
+		float orbitColorF[3];
+		unsigned int orbitColorI;
 	};
-	const Planets& GetPlanets(){
-		return m_planets;
-	};
+
+	typedef std::vector<std::pair<KeplerOrbits::Body, RenderProperties> > BodyList;
+
+	Resources();
+	
+	~Resources();
+	
+	void Load(const char* fileName);
+
+	const BodyList& GetBodies() const;
+
 private:
-	double GetVal(TiXmlHandle& handle, const char* name) const
-	{
-		double parametr;
-		handle.FirstChild( name ).Element()->QueryDoubleAttribute("value", &parametr);
-		return parametr;
-	};
-	double GetRVal(TiXmlHandle& handle, const char* name) const
-	{
-		return toRad( GetVal(handle, name) );
-	};
-	Planets m_planets;
+	int GetIVal(TiXmlHandle& handle, const char* name) const;
+	int GetHVal(TiXmlHandle& handle, const char* name) const;
+	double GetDVal(TiXmlHandle& handle, const char* name) const;
+	double GetRVal(TiXmlHandle& handle, const char* name) const;
+	
+	BodyList m_bodies;
 };
